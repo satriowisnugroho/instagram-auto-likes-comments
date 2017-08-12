663f0d160a836babd9991d3ec836fa1ee9a78265
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2017-08-12 23:36:27 +0700

    Update scrape logic.

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const path = require('path');
+const co = require('co');
 const Nightmare = require('nightmare');
 const router = express.Router();
 const nightmare = Nightmare({
-    show : true,
+    show: true,
     webPreferences: {
         partition: 'nopersist'
     },
 });
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../views/home.html'));
 });
 
-router.post('/', function(req, res) {
+router.post('/', function (req, res) {
 
     const username = req.body.username;
     const password = req.body.password;
@@ -21,42 +22,82 @@ router.post('/', function(req, res) {
     const comment = req.body.comment;
     const posts = req.body.posts;
 
-    nightmare
-        .goto('http://instagram.com')
-        .wait('a[href="javascript:;"]')
-        .click('a[href="javascript:;"]') // log in option
-        .wait('input[name=username]')
-        .insert('input[name=username]', username)
-        .insert('input[name=password]', password)
-        .click('button') // btn login
-        .wait('input[placeholder=Search]')
-        .insert('input[placeholder=Search]', search)
-        .wait('._gimca')
-        .click('._gimca') // top suggest
-        // .wait(`a[href*=${search}]`)
-        .wait(5000)
-        .end()
-        .wait('._mck9w')
-        .click('._mck9w a') // click post
-        .wait('._eszkz');
+    const searchSelector = 'input[placeholder=Search]';
+    const heartClass = 'coreSpriteHeartOpen';
+    const spriteComment = '.coreSpriteComment';
+    const commentSelector = 'textarea[placeholder="Add a comment…"]';
 
-    for (let i = 0; i < posts; i++) {
-        nightmare.wait(1000)
-            .click('._eszkz') // like
-            .insert('._bilrf', comment) // comment
-            .type('._bilrf', '\u000d')
-            .click('._3a693'); // next
+    function *run() {
+        const topPostClass = yield nightmare
+            .goto('http://instagram.com')
+            .wait('a[href="javascript:;"]')
+            .click('a[href="javascript:;"]') // log in option
+            .wait('input[name=username]')
+            .insert('input[name=username]', username)
+            .insert('input[name=password]', password)
+            .click('button') // login button
+            .wait(searchSelector)
+            .insert(searchSelector, search)
+            .wait(1000)
+            .evaluate(function (selector) {
+                const node = document.querySelector(selector).parentNode.childNodes[3];
+                return node.childNodes[1].childNodes[0].childNodes[0].className;
+            }, searchSelector);
+
+        yield nightmare.click(`.${topPostClass}`).wait('h2');
+
+        const postClass = yield nightmare
+            .evaluate(function () {
+                let position;
+                const a = document.getElementsByTagName('a');
+                for (let i = 0; i < a.length; i++) {
+                    const link = a[i].getAttribute('href');
+                    if (/\/p\//.test(link)) {
+                        position = i;
+                        break;
+                    }
+                }
+                return a[position].parentNode.className.split(' ')[0];
+            });
+
+        yield nightmare
+            .click(`.${postClass} a`) // click post
+            .wait(spriteComment);
+
+        const likeClass = yield nightmare
+            .evaluate(function (selector) {
+                const cls = document.querySelector(selector).parentNode.parentNode.childNodes[0].className;
+                return cls.split(' ')[0];
+            }, spriteComment);
+
+        for (let i = 0; i < posts; i++) {
+            nightmare.wait(1000);
+
+            const liked = yield nightmare
+                .evaluate(function (selector) {
+                    return document.querySelector(`.${selector}`).childNodes[0].className.split(' ')[1];
+                }, likeClass);
+
+            if (heartClass === liked) nightmare.click(`.${likeClass}`); // like
+            nightmare.insert(commentSelector, comment) // comment
+                .type(commentSelector, '\u000d')
+                .click('.coreSpriteRightPaginationArrow'); // next
+        }
+
+        nightmare
+            .wait(1000)
+            .end()
+            .then(function () {
+                res.json({ status: 'success' });
+            })
+            .catch(function () {
+                res.json({ status: 'error' });
+            });
     }
 
-    nightmare
-        .wait(1000)
-        .end()
-        .then(function () {
-            res.json({status:'success'});
-        })
-        .catch(function () {
-            res.json({status:'success'});
-        });
+    co(run).catch(function () {
+        res.json({ status: 'error' });
+    });
 });
 
 module.exports = router;
